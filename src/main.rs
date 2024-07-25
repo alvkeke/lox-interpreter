81749c39a1ad6cf4be1470b965c06d607a8dd410
alvkeke <alvkeke@126.com>
2024-07-25 15:00:53 +0800

    implement the usage in main()

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,8 @@
 
 use std::{self, io::{self, Write}};
 
+use syntax::statement::Stmt;
+
 mod syntax {
     pub mod token;
     pub mod expression;
@@ -23,10 +25,28 @@ fn main() {
         if let Err(errmsg) = syntax::token::scan_from_line(&input_buffer, &mut tokens) {
             eprintln!("failed to parse the input line: {}", errmsg);
         } else {
-            dbg!("{}", tokens);
+            dbg!(&tokens);
         };
 
-        break;  // for test
+        // let mut stmts: Vec<Stmt> = Vec::new();
+        // while let 
+        while !tokens.is_empty() {
+            match Stmt::stmt(&tokens, 0) {
+                Ok((stmt, used)) => {
+                    tokens.remove(used);
+                    match stmt.exec() {
+                        Err(msg) => println!("{}", msg),
+                        _ => {},
+                    }
+                },
+                Err(msg) => {
+                    println!("{}", msg);
+                    break;
+                }
+            }
+        }
+
+        // break;  // for test
     }
 
 }
